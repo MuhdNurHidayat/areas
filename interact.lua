fc3065431cdeb53f64db951d546a6009b50de32d
Jat15 <jinan15@hotmail.com>
2018-05-08 03:43:59 +0200

    Fix typo due to conflict

--- interact.lua
+++ interact.lua
@@ -14,7 +14,7 @@ minetest.register_on_protection_violation(function(pos, name)
 	if not areas:canInteract(pos, name) then
 		local owners = areas:getNodeOwners(pos)
 		--minetest.chat_send_player(name,	(S("%s is protected by %s.")):format(minetest.pos_to_string(pos), table.concat(owners, ", ")))
-		minetest.chat_send_player(name,(S("%s is protected by %s.")),minetest.pos_to_string(pos),table.concat(owners, ", ")))
+		minetest.chat_send_player(name,(S("%s is protected by %s.")),minetest.pos_to_string(pos),table.concat(owners, ", "))
 		minetest.after(1,anti_lag,{player=player,playerpos=playerpos})
 	end
 end)
